6997f70690552790b4ac496f46acf3024593ff3e
james <81617086+je-cook@users.noreply.github.com>
2024-09-18 14:48:06 +0100

    fix path save

--- studies/first/run.py
+++ studies/first/run.py
@@ -10,13 +10,13 @@ from stellarator_project.stellarator import main
 Path(Path(__file__).parent, "data/cad_output").mkdir(parents=True, exist_ok=True)
 Path(Path(__file__).parent, "data/coils").mkdir(parents=True, exist_ok=True)
 Path(Path(__file__).parent, "data/plasma").mkdir(parents=True, exist_ok=True)
-build_config_path = Path(__file__, "../config/config.json").resolve()
+build_config_path = Path(Path(__file__).parent, "config/config.json").resolve()
 
 reactor = main(build_config_path)
 reactor.show_cad()
 reactor.save_cad(
     "xyz",
     filename=Path(
-        Path(__file__).parent, "../data/cad_output/plasmastellarator.stp"
+        Path(__file__).parent, "data/cad_output/plasmastellarator.stp"
     ).resolve(),
 )
